99b4ebabc6c40df041d57d2c4f28c08b86ec6f9b
abdullahallababidy <145194063+gitAbdullahAllababidy@users.noreply.github.com>
2025-03-16 14:02:46 +0300

    Initialization commit

---
+++ type_ahead_search_mobile_app/README.md
@@ -0,0 +1,3 @@
+# type_ahead_search_mobile_app
+
+A new Flutter project.
---
+++ type_ahead_search_mobile_app/android/app/src/main/kotlin/com/example/type_ahead_search_mobile_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.type_ahead_search_mobile_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
